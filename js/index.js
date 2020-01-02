87ccf8dc2e6cd8da112e5530256193b5da49fe53
Shubham Khandalkar <shubham.sk473@gmail.com>
2020-01-02 16:19:45 +0530

    Adding greetings div
    Greetings div could not be centered

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 // initialization
 var canvas = document.getElementById("canvas");
-
+var home = document.getElementById("home");
 function resetCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.rect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#252934";
-  ctx.fill();
+	home.height = window.innerHeight; 
+   
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.rect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#252934";
+	ctx.fill();
 }
 
 resetCanvas();
