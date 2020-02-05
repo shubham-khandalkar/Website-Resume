eaa1c43d218dd7efde159509251124d7e2464ae6
Shubham Khandalkar <shubham.sk473@gmail.com>
2020-02-05 11:20:37 +0530

    Animations added

--- js/css-animation.js
+++ js/css-animation.js
@@ -1,9 +1,6 @@
 $(".trigger-animated").inViewport(function(px) {
   if (px) {
+	  
     $(this).addClass("triggeredCSS3");
-    var element = $(this);
-    setTimeout(function() {
-      element.css("visibility", "visible");
-    }, 700);
   }
 });
